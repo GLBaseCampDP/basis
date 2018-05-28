0d1bb69120259f3ae49505936504fe4d2a4ec654
win_dell <win_dell@WIN_DELL>
2018-05-28 16:56:53 +0300

    basis start

--- basis/basis.cpp
+++ basis/basis.cpp
@@ -3,6 +3,17 @@
 
 #include "stdafx.h"
 
+#define F(name, val) int f_##name() { return val;}
+
+F(1, 1) // replaced by f_1(){return 1;}
+F(2, 2)// replaced by f_2(){return 1;}
+
+#define FF(name, ...) int ff_##name(int __VA_ARGS__) { return 0;}
+FF(a, b) // replaced by ff_a(0, int b,){return 0;}
+
+#define FFF(...) int fff(__VA_ARGS__) { return 0;}
+FFF(int k, int z) // replaced by fff(int k, int z) {return 0;}
+
 
 int main()
 {
